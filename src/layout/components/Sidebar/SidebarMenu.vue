cdaf34913ee7e70f8665c4778b5309e52bf7358a
ljx <452363993@qq.com>
2022-04-20 13:16:52 +0800

    feat(layout/Sidebar/SidebarMenu.vue): 修改样式为全局可变样式，为主题控制铺路

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -2,10 +2,12 @@
   <!-- 一级 menu 菜单 -->
   <el-menu
     :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :default-active="activeMenu"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true"
+    router
   >
     <sidebar-item v-for="item in routes" :key="item.path" :route="item">
     </sidebar-item>
@@ -14,15 +16,26 @@
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
 import SidebarItem from './SidebarItem.vue'
 
+// 获取路由表结构
 const router = useRouter()
 // 获取符合动态menu菜单需要的结构化路由表数据
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
+})
+
+// 计算高亮menu
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { meta, path } = route
+  if (meta.activeMenu) {
+    return meta.activeMenu
+  }
+  return path
 })
 </script>
 
