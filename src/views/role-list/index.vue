5594d4abc18273fb92f0d730c78e6673def9830b
ljx <452363993@qq.com>
2022-05-05 09:55:24 +0800

    feat: 功能权限控制实现

--- src/views/role-list/index.vue
+++ src/views/role-list/index.vue
@@ -18,6 +18,7 @@
             type="primary"
             size="mini"
             @click="onDistributePermissionClick(row)"
+            v-permission="['distributePermission']"
           >
             {{ $t('msg.role.assignPermissions') }}
           </el-button>
